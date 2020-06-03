c943973f4b2fd7fc9ac118d0a07b1d7c4b737822
magaum <weslei.paula@fatec.sp.gov.br>
2020-06-03 01:17:16 -0300

    - Operações de CRUD refatoradas; - Adicionada funcionalidade de restauração de contato; - Adicionada paginação na funcionalidade de restauração de contato.

--- infrastructure/contatoRepository.py
+++ infrastructure/contatoRepository.py
@@ -12,14 +12,17 @@ class ContatoRepository:
     def connect(self):
         self.conexao = sqlite3.connect("contatos.db")
         self.cursor = self.conexao.cursor()
+        self.createIfNotExists()
 
     def createIfNotExists(self):
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS contatos (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
-            celular INTEGER NOT NULL,
             email TEXT,
+            ddd TEXT NOT NULL,
+            celular TEXT NOT NULL,
+            isDeleted BOOLEAN DEFAULT 0,
             UNIQUE(id))''')
         self.conexao.commit()
 
@@ -27,15 +30,31 @@ class ContatoRepository:
         nome = 0
         celular = 1
         email = 2
-        self.cursor.execute('INSERT INTO contatos (id, nome, celular, email) VALUES(NULL, ?, ?, ?)',
-                            (contato[nome], contato[celular], contato[email]))
+        self.cursor.execute('INSERT INTO contatos (id, nome, ddd, celular, email) VALUES(NULL, ?, ?, ?, ?)',
+                            (contato.nome, contato.ddd, contato.celular, contato.email))
         self.conexao.commit()
 
+    def findById(self, id):
+        return self.cursor.execute(f'SELECT * FROM contatos WHERE id = ? AND isDeleted = 0', (id,))
+
+    def exists(self, id):
+        return self.cursor.execute(f'SELECT EXISTS(SELECT 1 FROM contatos WHERE id = ?)', (id,)).fetchone()[0]
+
+    def findContatosExcluidos(self, pagina=0, tamanho=10):
+        skipItems = self.skip(pagina, tamanho)
+        return self.cursor.execute(f'''SELECT * FROM contatos
+                                    WHERE IsDeleted = 1
+                                    ORDER BY id
+                                    LIMIT {tamanho}
+                                    OFFSET {skipItems}''').fetchall()
+
     def query(self):
-        return self.cursor.execute('SELECT * FROM contatos ORDER BY nome DESC LIMIT 1 OFFSET 1').fetchall()
+        return self.cursor.execute(f'''SELECT * FROM contatos
+                                    WHERE IsDeleted = 0
+                                    ORDER BY id''').fetchall()
 
     def findIds(self):
-        return self.cursor.execute("SELECT id column from contatos").fetchall()
+        return self.cursor.execute("SELECT id column from contatos WHERE IsDeleted = 0").fetchall()
 
     def deleteById(self, id):
         """
@@ -43,6 +62,19 @@ class ContatoRepository:
          :param id: Id do contato
          :return:
          """
-        print(id)
-        self.cursor.execute('DELETE from contatos WHERE id = ?', (id,))
+        self.cursor.execute(
+            'UPDATE contatos SET IsDeleted = 1 WHERE id = ?', (id,))
         self.conexao.commit()
+
+    def restaurar(self, id):
+        """
+        Restaura contato por id
+        :param id: Id do contato
+        :return:
+        """
+        self.cursor.execute(
+            'UPDATE contatos SET IsDeleted = 0 WHERE id = ?', (id,))
+        self.conexao.commit()
+
+    def skip(self, pagina, tamanho):
+        return pagina * tamanho
